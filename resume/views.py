9bf06c619147f6fae87dc3eaf1d754885bfd57f7
Hani Rahimi <136378674+hanoRahimi@users.noreply.github.com>
2024-08-11 15:25:08 +0330

    created cv
    created cv project and resume app and settings.
    dynamic address
    edit index and resume about pages

--- resume/views.py
+++ resume/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index_view(request):
+    return render(request, 'resume/index.html')
+
+def about_view(request):
+    return render(request, 'resume/about.html')
+
+def resume_view(request):
+    return render(request, 'resume/resume.html')
+
+def services_view(request):
+    return render(request, 'resume/services.html')
+
+def portfolio_view(request):
+    return render(request, 'resume/portfolio.html')
+
+def contact_view(request):
+    return render(request, 'resume/contact.html')
